dbbe1da0e57ffb143d2268124efcd3bee08b5d9c
Aleksandra Izhetnikova <izhetnikovaalexandra@yandex.ru>
2023-04-25 22:21:37 +0300

    Update User model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,21 +3,55 @@ from django.db import models
 
 
 class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
     ROLES = (
-        ('user', 'user'),
-        ('moderator', 'user'),
-        ('admin', 'admin'),
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
     )
 
-    username = models.CharField(max_length=100, null=True, unique=True)
-    email = models.EmailField(unique=True)
-    role = models.CharField(max_length=50, choices=ROLES, null=True)
-    bio = models.TextField(
-        'Биография',
-        blank=True,
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150,
+        null=True,
+        unique=True
     )
-    first_name = models.CharField(max_length=100, null=True)
-    last_name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        max_length=254,
+        unique=True
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=50,
+        choices=ROLES,
+        null=True
+    )
+    bio = models.TextField(
+        verbose_name='О себе',
+        blank=True,
+        null=True
+    )
+    first_name = models.CharField(max_length=150, null=True)
+    last_name = models.CharField(max_length=150, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+    
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'ПОльзователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
-        return self.username
+        return self.email
