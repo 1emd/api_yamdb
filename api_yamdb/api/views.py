ece1579d2334710eba0429a8360f84365e05f8ef
Кирилл Хорошилов <khoroshilovkirill@gmail.com>
2023-04-27 23:09:14 +0300

    добавил view, mix, perm, serial

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+
+from api.serializers import (
+    CategorySerializer, GenreSerializer,
+    TitleSerializer, TitleCreateUpdateSerializer)
+from api.permissions import IsAdminUserOrReadOnly
+from api.mixins import CreateListDeleteViewSet
+from api.filters import TitleFilter
+from reviews.models import Category, Genre, Title
+
+
+class CategoryViewSet(CreateListDeleteViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewSet(CreateListDeleteViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class TitleViewSet(ModelViewSet):
+    queryset = Title.objects.all().annotate(
+        rating=Avg('reviews__score')).all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'partial_update'):
+            return TitleCreateUpdateSerializer
+        return TitleSerializer
