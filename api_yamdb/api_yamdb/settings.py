1d33fe3aec8025a3c0116d72d38a4742daa04c2a
Кирилл Хорошилов <khoroshilovkirill@gmail.com>
2023-04-28 01:30:42 +0300

    добавил view и поправил sett

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -16,15 +16,16 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
-    'api',
-    'reviews',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework'
+    'django_filters',
 ]
 
 MIDDLEWARE = [
