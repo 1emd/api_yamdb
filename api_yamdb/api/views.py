eeaa4e798344bb2af2d27393f51c70a96411a3b8
Aleksandra Izhetnikova <izhetnikovaalexandra@yandex.ru>
2023-04-28 00:31:46 +0300

    Add UserViewSet and email and token functions

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action, api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
 
-# Create your views here.
+from api.serializers import UserSerializer, EmailSerializer, TokenSerializer
+from users.models import User
+from api.permissions import IsAdmin
+
+
+@api_view(['POST'])
+def registration(request):
+    serializer = EmailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data.get('username')
+    email = serializer.validated_data.get('email')
+    user = get_object_or_404(User, email=email, username=username)
+    confirmation_code = default_token_generator.make_token(user)
+    send_mail(
+        subject='Регистрация YaMDB',
+        message=f'Ваш код подтверждения: {confirmation_code}',
+        from_email='from@example.com',
+        recipient_list=[email],  # Это поле "Кому" (можно указать список адресов)
+        fail_silently=False, # Сообщать об ошибках («молчать ли об ошибках?»)
+    )
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def get_token(request):
+    serializer = TokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    email = serializer.validated_data.get('email')
+    user = get_object_or_404(User, email=email)
+    confirmation_code = serializer.validated_data.get('confirmation_code')
+    if default_token_generator.check_token(user, confirmation_code):
+        token = AccessToken.for_user(user)
+        return Response({'token': token}, status=status.HTTP_200_OK)
+    return Response(
+        {'confirmation_code': confirmation_code},
+        status=status.HTTP_400_BAD_REQUEST
+    )
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdmin,)
+    filter_backends = [filter.SearchFilter]
+    search_fields = ('user__username',)
+
+    @action(
+        methods=['patch', 'get'],
+        permission_classes=(IsAuthenticated,),
+        detail=False,
+        url_path='me',
+        url_name='me'
+    )
+    def me(self, request):
+        user = self.request.user
+        serializer = self.get_serializer(user)
+        if self.request.method == 'PATCH':
+            serializer = self.get_serializer(
+                user,
+                data=request.data,
+                partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data)
